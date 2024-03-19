83e99563058779b24961433c701f1d512ba812ce
Gianina Borsan <Gianina.Borsan@rinf.tech>
2024-03-19 21:11:29 +0200

    remove redundant files after the merge was done

--- src/Common/Communication.Contracts/VisitorTrackedEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Communication.Contracts
-{
-    public class VisitorTrackedEvent
-    {
-        public string? Referer { get; init; }
-        public string? UserAgent { get; init; }
-        public string IpAddress { get; init; }
-    }
-}
