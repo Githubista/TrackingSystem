5190d5f1b14a211a8fd234c2e6acf914a5c76cff
Gianina Borsan <Gianina.Borsan@rinf.tech>
2024-03-19 21:07:22 +0200

    Enhance docker-compose to be able to test services run with docker

--- src/Services/PixelService/Tracking/TrackingService.cs
+++ src/Services/PixelService/Tracking/TrackingService.cs
@@ -18,8 +18,10 @@ namespace PixelService.Tracking
 
         public ValueTask<TrackingResponseModel> GetAsync(TrackingRequestModel request, HttpContext context, CancellationToken cancellationToken)
         {
-            //this is a fire and forget method as my assumption is that we need to return very fast a response to this method
-            // so we can save time by not awaiting this publish
+            // this is a fire and forget method as my assumption is that we need to return very fast a response to this method
+            // so we can save time by not awaiting this publish;
+            // also, if there are any problems with the communication with StorageService we still want to be able to return a result
+            // and not be blocked by this step
             _ = PublishTrackingInformation(request, cancellationToken);
             return ValueTask.FromResult(new TrackingResponseModel(ImageLoader.TrackingImage));
         }
